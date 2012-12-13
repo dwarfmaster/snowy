07692094d5ae281e703c8c8261e75154afa1cd7e
lucas8 <luc.linux@mailoo.org>
2012-12-13 17:28:03 +0100

    Création de l'interface de l'écran.

---
+++ screen.hpp
@@ -0,0 +1,18 @@
+
+#ifndef DEF_SCREEN
+#define DEF_SCREEN
+
+class Screen
+{
+	public:
+		Screen();
+		~Screen();
+
+		void print(const char* txt);
+		void refresh();
+
+	private:
+};
+
+#endif//DEF_SCREEN
+
