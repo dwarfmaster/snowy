5e6ce7eb43d12641188ea3e6dddb926a04410eed
lucas8 <luc.linux@mailoo.org>
2012-12-20 17:28:05 +0100

    Correction d'une erreur dans la génération du code de la musique.

--- genMusicHpp.py
+++ genMusicHpp.py
@@ -7,10 +7,11 @@ hpp.write("#ifndef DEF_MUSIC\n#define DEF_MUSIC\n\nenum Notes{DO,RE,MI,FA,SOL,LA
 
 with open('music','r') as csvfile:
 	reader = csv.reader(csvfile,delimiter='\t')
+	i = 0
 	for row in reader:
 		if row[0] == "TEMPO":
 			hpp.write(row[1])
-			hpp.write(";\nstruct Note { Notes note; unsigned int duration; float repeat; };\nNote music = {\n")
+			hpp.write(";\nstruct Note { Notes note; unsigned int duration; float repeat; };\nNote music[] = {\n")
 		else:
 			hpp.write("\t{")
 			hpp.write(row[0])
@@ -19,7 +20,10 @@ with open('music','r') as csvfile:
 			hpp.write(",")
 			hpp.write(row[2])
 			hpp.write("},\n")
-	hpp.write("};")
+			i+=1
+	hpp.write("};\nunsigned int sizeMus = ")
+	hpp.write(str(i))
+	hpp.write(";\n")
 
 hpp.write("\n\n#endif")
 hpp.close()
