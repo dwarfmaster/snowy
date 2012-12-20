9f6fa045b919d16bb9a3b702f8772218ee576916
lucas8 <luc.linux@mailoo.org>
2012-12-20 17:02:19 +0100

    Correction d'un oublit de ; dans la génération du code de musique.

--- genMusicHpp.py
+++ genMusicHpp.py
@@ -3,14 +3,14 @@
 import csv
 
 hpp = open('music.hpp', 'w')
-hpp.write("#ifndef DEF_MUSIC\n#define DEF_MUSIC\n\nenum Notes{DO,RE,MI,FA,SOL,LA,SI,NO};\nconst unsigned int tempo =")
+hpp.write("#ifndef DEF_MUSIC\n#define DEF_MUSIC\n\nenum Notes{DO,RE,MI,FA,SOL,LA,SI,NO};\nconst unsigned int tempo = ")
 
 with open('music','r') as csvfile:
 	reader = csv.reader(csvfile,delimiter='\t')
 	for row in reader:
 		if row[0] == "TEMPO":
 			hpp.write(row[1])
-			hpp.write("\nstruct Note { Notes note; unsigned int duration; float repeat; };\nNote music = {\n")
+			hpp.write(";\nstruct Note { Notes note; unsigned int duration; float repeat; };\nNote music = {\n")
 		else:
 			hpp.write("\t{")
 			hpp.write(row[0])
