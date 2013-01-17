2de857cf9f054e272e59c60f4e169bff1fc07c0d
lucas8 <luc.linux@mailoo.org>
2013-01-17 17:34:19 +0100

    Définition des pins de l'écran.

--- pins.hpp
+++ pins.hpp
@@ -4,6 +4,13 @@
 
 // Les pins avec leur rôle
 const int pinMus = 2;
+const int pinRetro = 3;
+const int pinScrRS = 12;
+const int pinScrEn = 11;
+const int pinScr1 = 4;
+const int pinScr2 = 5;
+const int pinScr3 = 6;
+const int pinScr4 = 7;
 
 #endif//DEF_PINS
 
