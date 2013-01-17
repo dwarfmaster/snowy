18eda6302ab0e5862b896caeb2e878472873cda5
lucas8 <luc.linux@mailoo.org>
2013-01-17 16:25:24 +0100

    Début de l'implémentation de Sound.

--- sound.cpp
+++ sound.cpp
@@ -1,4 +1,58 @@
 
 #include "sound.hpp"
+#include "music.hpp"
+
+// Internal functions
+void Sound::playNote(Notes note, unsigned int duration)
+{
+}
+
+// Interface functions
+Sound::Sound()
+	: m_vol(5), m_paused(true), m_pos(0)
+{
+	// TODO initaliser beeper
+}
+
+Sound::~Sound()
+{
+	// TODO libérer beeper
+}
+
+void Sound::play()
+{
+	m_paused = false;
+	m_firstTime = millis();
+	m_lastTime = millis()
+	m_vol = 125;
+	// TODO lancer musique
+}
+
+void Sound::update()
+{
+	if( m_paused )
+		return;
+
+	m_vol = (millis() - m_firstTime()) * (130/10000);
+	m_vol += 125;
+	if( m_vol > 255 ) m_vol = 255;
+
+	unsigned int timeSpent = millis() - m_lastTime;
+	while( timeSpent <= timeStay )
+	{
+	}
+}
+
+void Sound::stop()
+{
+	m_paused = true;
+	m_firsttime = 0;
+	// TODO stopper musique
+}
+
+void Sound::setvolume(int vol)
+{
+	m_vol = vol;
+}
 
 
--- sound.hpp
+++ sound.hpp
@@ -2,7 +2,9 @@
 #ifndef DEf_SOUND
 #define DEF_SOUND
 
+#include "Arduino.h"
 #include "pins.hpp"
+enum Notes;
 
 class Sound
 {
@@ -11,11 +13,20 @@ class Sound
 		~Sound();
 
 		void play();
+		void update();
 		void stop();
 		void setvolume(int vol);
 
 	private:
 		int m_vol;
+		bool m_paused;
+
+		unsigned long m_lastTime;
+		unsigned long m_firstTime;
+		unsigned int m_pos;
+		unsigned int m_timeStay;
+
+		void playNote(Notes note, unsigned int duration);
 };
 
 #endif//DEF_SOUND
