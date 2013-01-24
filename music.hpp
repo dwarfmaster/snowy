fe9e62e22f0b79d5215ac026baebe076dfc3da2d
lucas8 <luc.linux@mailoo.org>
2013-01-24 16:39:04 +0100

    Mise à jour de la partition.

--- music.hpp
+++ music.hpp
@@ -3,7 +3,7 @@
 
 enum Notes{DO,RE,MI,FA,SOL,LA,SI,NO};
 extern const unsigned int tempo;
-struct Note { Notes note; unsigned int duration; float repeat; };
+struct Note { Notes note; unsigned int repeat; float duration; };
 extern Note music[];
 extern unsigned int sizeMus;
 
