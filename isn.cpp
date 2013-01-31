c2deb116eed87352b0558b6b94718bfa16af0ca6
lucas8 <luc.linux@mailoo.org>
2013-01-31 14:25:35 +0100

    Implémentation du setup et du loop.

--- isn.cpp
+++ isn.cpp
@@ -1,14 +1,23 @@
 #include "Arduino.h"
+#include "reveil.hpp"
+#include "sound.hpp"
+#include "screen.hpp"
 
 void setup();
 void loop();
 
+Screen scr;
+Sound snd;
+Reveil reveil(&scr, &snd);
+
 void setup()
 {
+	instance = &reveil;
 }
 
 void loop()
 {
+	reveil.update();
 }
 
 
