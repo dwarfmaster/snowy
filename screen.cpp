2fa04482ad5cd8805e4aca4dbc0c4335df5ef5ce
lucas8 <luc.linux@mailoo.org>
2013-02-17 11:16:32 +0100

    Ajout de charactère spéciaux pour la sonnerie et l'édition.

--- screen.cpp
+++ screen.cpp
@@ -5,6 +5,42 @@ LiquidCrystal _lcd(pinScrRS, pinScrEn, pinScr1, pinScr2, pinScr3, pinScr4);
 
 void Screen::load()
 {
+	// Lettre cloche (0)
+	byte cloche[8] = {
+		B00100,
+		B01110,
+		B01010,
+		B11011,
+		B10001,
+		B10101,
+		B00100,
+	};
+	_lcd.createChar(0, cloche);
+
+	// Lettre config (1)
+	byte cfg[8] = {
+		B01110,
+		B01110,
+		B01110,
+		B01110,
+		B01110,
+		B00110,
+		B00100,
+	};
+	_lcd.createChar(1, cfg);
+
 	_lcd.begin(16,2);
+}
+
+void Screen::printE(unsigned int x, unsigned int y)
+{
+	_lcd.setCursor(x, y);
+	_lcd.write((uint8_t)1);
+}
+
+void Screen::printB(unsigned int x, unsigned int y)
+{
+	_lcd.setCursor(x, y);
+	_lcd.write((uint8_t)0);
 }
 
--- screen.hpp
+++ screen.hpp
@@ -12,12 +12,14 @@ class Screen
 {
 	public:
 		void load();
+		void printE(unsigned int x = 0, unsigned int y = 0);
+		void printB(unsigned int x = 0, unsigned int y = 0);
 		template<typename T>
-		void print(T txt, unsigned int x = 0, unsigned int y = 0)
-		{
-			_lcd.setCursor(x, y);
-			_lcd.print(txt);
-		}
+			void print(T txt, unsigned int x = 0, unsigned int y = 0)
+			{
+				_lcd.setCursor(x, y);
+				_lcd.print(txt);
+			}
 };
 
 #endif//DEF_SCREEN
