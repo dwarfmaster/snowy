28ef9f1b939a50898ad4696aa61ea8acca1259de
lucas8 <luc.linux@mailoo.org>
2013-02-21 17:48:25 +0100

    Ajout des informations de licence dans le code.

--- screen.hpp
+++ screen.hpp
@@ -1,4 +1,25 @@
 
+/*
+ * Copyright (C) 2013 Chabassier Luc
+ * Copyright (C) 2013 Héloïse Badaroux
+ * Copyright (C) 2013 Marie Qiu
+ * Copyright (C) 2013 Marie Elisabelar
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #ifndef DEF_SCREEN
 #define DEF_SCREEN
 
